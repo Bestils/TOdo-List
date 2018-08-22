7e918c7c03659b27cc2f53e0b1fa6f92ef421730
Adrian Nidzgorski <nidzgorskiadrian@gmail.com>
2018-08-22 21:57:14 +0200

    Basic configuration for spring security user login

--- src/main/java/pl/java/learning/todolist/infrastructure/config/SecurityConfig.java
+++ src/main/java/pl/java/learning/todolist/infrastructure/config/SecurityConfig.java
@@ -1,20 +1,56 @@
 package pl.java.learning.todolist.infrastructure.config;
 
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
-  protected void configure(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity
-        .authorizeRequests()
-        .anyRequest().permitAll()
+  protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("admin")
+        .password(
+            passwordEncoder()
+                .encode("admin")
+        ).
+        roles("ADMIN")
         .and()
+        .withUser("user")
+        .password(
+            passwordEncoder()
+                .encode("admin")
+        ).
+        roles("USER");
+
+  }
+
+  @Override
+  protected void configure(final HttpSecurity http) throws Exception {
+    http
         .csrf().disable()
-        .headers().frameOptions().disable();
+        .headers().frameOptions().disable()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/login*").anonymous()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+        .and()
+        .logout();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
--- src/main/resources/db/demo-data/V999.0.0.01__demo_users.sql
+++ src/main/resources/db/demo-data/V999.0.0.01__demo_users.sql
@@ -1,1 +1,2 @@
 insert into user values (1, 0, 'admin', 'admin', 'admin@admin.com');
+insert into user values (2, 0, 'user', 'admin', 'admin@admin.com');
